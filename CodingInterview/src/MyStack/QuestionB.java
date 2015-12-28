76ba40ea415eb679f9120e217a7ffa428e8fb1be
nickj <nickj@DESKTOP-0E6S0UO>
2015-12-29 00:12:54 +0900

    mod

--- CodingInterview/src/MyStack/QuestionB.java
+++ CodingInterview/src/MyStack/QuestionB.java
@@ -4,28 +4,28 @@ public class QuestionB {
 	static int number_of_stacks = 3;
 	static int default_size = 4;
 	static int total_size = default_size * number_of_stacks;
-	static StackData[] stacks = {new StackData(0, default_size),
+	StackData[] stacks = {new StackData(0, default_size),
 	new StackData(default_size, default_size),
 	new StackData(default_size * 2, default_size)
 	};
-	static int[] buffer = new int[total_size];
+	 int[] buffer = new int[total_size];
 	
 	// 모든 요소들의 크기
-	public static int numberOfElements(){
+	public int numberOfElements(){
 		return stacks[0].size + stacks[1].size + stacks[2].size; 
 	}
 	
-	public static int nextElement(int index){
+	public int nextElement(int index){
 		if(index + 1 == total_size) return 0;
 		else return index + 1;
 	}
 	
-	public static int previousElement(int index){
+	public int previousElement(int index){
 		if(index == 0) return total_size - 1;
 		else return index + 1;
 	}
 	
-	public static void shift(int stackNum){
+	public void shift(int stackNum){
 		StackData stack = stacks[stackNum];
 		if(stack.size >= stack.capacity){
 			int nextStack = (stackNum + 1) % number_of_stacks;
@@ -46,12 +46,12 @@ public class QuestionB {
 	}
 	
 	// Expand stack by shifting over other stacks
-	public static void expand(int stackNum){
+	public void expand(int stackNum){
 		shift((stackNum + 1) % number_of_stacks);
 		stacks[stackNum].capacity++;
 	}
 	
-	public static void push(int stackNum, int value) throws Exception{
+	public void push(int stackNum, int value) throws Exception{
 		StackData stack = stacks[stackNum];
 		// 남은 공간이 있는지 검사
 		if(stack.size >= stack.capacity){
@@ -66,7 +66,7 @@ public class QuestionB {
 		buffer[stack.pointer] = value;
 	}
 	
-	public static int pop(int stackNum) throws Exception{
+	public int pop(int stackNum) throws Exception{
 		StackData stack = stacks[stackNum];
 		if(stack.size == 0){
 			throw new Exception("Trying to pop an empty stack");
@@ -78,12 +78,12 @@ public class QuestionB {
 		return value;
 	}
 	
-	public static int peek(int stackNum){
+	public int peek(int stackNum){
 		StackData stack = stacks[stackNum];
 		return buffer[stack.pointer];
 	}
 	
-	public static boolean isEmpty(int stackNum){
+	public boolean isEmpty(int stackNum){
 		StackData stack = stacks[stackNum];
 		return stack.size == 0;
 	}
--- CodingInterview/src/ch03/Ex3_1_2.java
+++ CodingInterview/src/ch03/Ex3_1_2.java
@@ -1,7 +1,35 @@
 package ch03;
 
+import MyStack.QuestionB;
+
 public class Ex3_1_2 {
 	public static void main(String[] agrs){
+		QuestionB stack = new QuestionB();
+		
+		try {
+			stack.push(0, 0);	
+			stack.push(0, 1);	
+			stack.push(0, 2);
+			stack.push(0, 3);
+			stack.push(0, 7);
+			
+			stack.push(1, 4);	
+			stack.push(1, 5);	
+			stack.push(1, 6);
+			//stack.push(1, 7);
+			
+			stack.push(2, 8);	
+			stack.push(2, 9);	
+			stack.push(2, 10);
+			stack.push(2, 11);
+			
+			System.out.println(stack.peek(0));
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+		
 		
 	}
 }
