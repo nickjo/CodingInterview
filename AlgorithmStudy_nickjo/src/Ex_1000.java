9dee71fa4af31bb05ac349b90ace8f8c0e6352be
nickj <nickj@DESKTOP-0VTTNR4>
2016-03-03 14:33:01 +0900

    add

---
+++ AlgorithmStudy_nickjo/src/Ex_1000.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex_1000 {
+	public static void main(String[] args) {
+		int a,b;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		a = Integer.parseInt(scanner.next());
+		b = Integer.parseInt(scanner.next());
+		
+		System.out.println(a+b);
+	}
+}
