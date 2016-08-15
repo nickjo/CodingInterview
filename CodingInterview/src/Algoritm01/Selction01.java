4507eaf95958eba8f0ae72335fede0caea71fc4f
origin <nickjo180@gmail.com>
2016-08-15 23:49:12 +0900

    add selection01 class

---
+++ CodingInterview/src/Algoritm01/Selction01.java
@@ -0,0 +1,10 @@
+package Algoritm01;
+
+/**
+ * Created by nick on 16. 8. 15.
+ */
+public class Selction01 {
+    public static void main(String[] args) {
+        System.out.println("Selection01!!!");
+    }
+}
