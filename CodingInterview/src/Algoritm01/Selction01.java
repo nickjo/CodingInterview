0338b237fcff575907ed5a82ef08c3d6b305e7f9
origin <nickjo180@gmail.com>
2016-08-17 03:29:45 +0900

    Complete Section01

--- CodingInterview/src/Algoritm01/Selction01.java
+++ CodingInterview/src/Algoritm01/Selction01.java
@@ -1,10 +1,29 @@
 package Algoritm01;
 
+import java.util.Scanner;
+
 /**
  * Created by nick on 16. 8. 15.
  */
 public class Selction01 {
     public static void main(String[] args) {
-        System.out.println("Selection01!!!!!!!");
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("======a: Decimal, b: Hex======");
+        for (;;){
+            String input1 = s.nextLine();
+
+            if(input1.equals("a")){ // Decimal
+                System.out.println("Decimal Number");
+                System.out.println(s.nextInt());
+                break;
+            }else if (input1.equals("b")){ // Hex
+                System.out.println("Hex Number");
+                System.out.println(Integer.toHexString(s.nextInt()));
+                break;
+            }else{
+                System.out.println("Wrong Input!!!");
+            }
+        }
     }
 }
